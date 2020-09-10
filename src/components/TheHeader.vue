8f7fe5a74ef4dd1bc7581e3ad180a1787546fb31
villasurlamer <loic.dekester@gmail.com>
2020-09-09 19:38:11 -0700

    created basic vue

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <nav>
+        <router-link to="/" class="brand">
+          Home
+        </router-link>
+        <router-link to="/leaflet" class="brand">
+          Leaflet
+        </router-link>
+        <router-link to="/openlayer" class="brand">
+          OpenLayer
+        </router-link>
+  </nav>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="home-page">
+    <div>
+      <div class="flex">
+        <h1 class="full">Home</h1>
+        <h2 class="full">This is the home page</h2>
+      </div>
+      <div class="flex three">
+        <div></div>
+        <div>
+          <article class="card">
+            <img src="../../public/mistyForest.jpg">
+            <header>
+              <h3>Maps</h3>
+            </header>
+            <footer>
+              <p>
+              This app is an example of how to use different map API<br>
+              You can use the header to see the result with different libraries<br>
+              Such as Leaflet or OpenLayer libraries.<br>
+              </p>
+            </footer>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomeVue",
+};
+</script>
+
+<style scoped>
+.home-page {
+  padding: 80px 0 0;
+  margin: 0 6em;
+}
+</style>
