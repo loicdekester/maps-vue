61073abf301a1b6242fa6cfdbfe004194adc844d
villasurlamer <loic.dekester@gmail.com>
2020-09-11 20:24:17 -0700

    added close function for popup

--- src/views/OpenLayer.vue
+++ src/views/OpenLayer.vue
@@ -2,7 +2,8 @@
   <section class="openlayer-page">
     <div id="map"></div>
     <!-- Popup -->
-    <div id="popup" class="ol-popup">
+    <div v-if="openPopUp" id="popup" class="ol-popup">
+      <div id="popup-closer" class="ol-popup-closer" @click="closePopUp"></div>
       <div id="popup-content"></div>
     </div>
   </section>
@@ -17,6 +18,7 @@ export default {
   data() {
     return {
       map: null,
+      openPopUp:true
     }
   },
   mounted() {
@@ -26,6 +28,9 @@ export default {
     mapInitialize() {
       OLService.createOpenLayerMap();
     },
+    closePopUp() {
+      this.openPopUp = false;
+    }
   }
 }
 </script>
@@ -46,8 +51,40 @@ export default {
   padding: 15px;
   border-radius: 10px;
   border: 1px solid #cccccc;
-  bottom: 40px;
+  bottom: 50px;
+  left: -50px;
   min-width: 280px;
   font-size: 12px;
+}
+.ol-popup:after,
+.ol-popup:before {
+  top: 100%;
+  border: solid transparent;
+  content: " ";
+  height: 0;
+  width: 0;
+  position: absolute;
+  pointer-events: none;
+}
+.ol-popup:after {
+  border-top-color: white;
+  border-width: 10px;
+  left: 48px;
+  margin-left: -10px;
+}
+.ol-popup:before {
+  border-top-color: #cccccc;
+  border-width: 11px;
+  left: 48px;
+  margin-left: -11px;
+}
+.ol-popup-closer {
+  text-decoration: none;
+  position: absolute;
+  top: 2px;
+  right: 8px;
+}
+.ol-popup-closer:after {
+  content: "✖";
 }
 </style>
